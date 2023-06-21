d71758b00d77b2b8f1f4500e61b50b220d093818
Joris <440484@student.fontys.nl>
2023-06-21 19:39:53 +0200

    final admin panel styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   has_many :orders, through: :orders_products
   validates :name, presence: true
   validates :price, presence: true
+  validates :available_amount, presence: true
 end
